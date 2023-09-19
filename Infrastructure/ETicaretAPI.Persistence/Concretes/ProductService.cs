94fd52c48065c8c6fdca0afd71ab7b55b463ef64
tarhan84 <tarhan.werek@gmail.com>
2023-09-19 14:28:27 +0300

    202309191428

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new()
+            {
+                new() {Id = Guid.NewGuid(), Name = "Product 1", Price = 456.30, Stock = 50},
+                new() {Id = Guid.NewGuid(), Name = "Product 2", Price = 100.00, Stock = 100},
+                new() {Id = Guid.NewGuid(), Name = "Product 3", Price = 45.00, Stock = 2000},
+                new() {Id = Guid.NewGuid(), Name = "Product 4", Price = 250.74, Stock = 120},
+                new() {Id = Guid.NewGuid(), Name = "Product 5", Price = 120.00, Stock = 70},
+            };
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productService.GetProducts());
+        }
+    }
+}
