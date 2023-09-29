48a4a2e144e62255558c87523bf4c1bebe489706
tarhan84 <tarhan.werek@gmail.com>
2023-09-29 10:03:51 +0300

    202309291003 add validators for product

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductModelValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Models.Products;
+using FluentValidation;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductModelValidator : AbstractValidator<CreateProductModel>
+    {
+        public CreateProductModelValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Product name cannot be null!")
+                .MaximumLength(150)
+                .MinimumLength(2)
+                    .WithMessage("Product name leng must be between 2-150!");
+
+            RuleFor(p => p.Stock)
+                .NotNull()
+                    .WithMessage("Product stock cannot be null!")
+                .Must(p => p >= 0)
+                .WithMessage("Product stock must greater or equals zero");
+
+            RuleFor(p => p.Price)
+                .NotNull()
+                    .WithMessage("Product price cannot be null!")
+                .Must(p => p >= 0)
+                .WithMessage("Product price must greater or equals zero");
+        }
+    }
+}
